6686bd9fed61e138615a65d5bf0f8e37472cb6b2
HARSHITA GOUR <143824677+happywithu21@users.noreply.github.com>
2026-08-04 09:34:14 +0530

    Initial commit

---
+++ app.java
@@ -0,0 +1,6 @@
+public class app {
+    public static void main(String[] args) {
+        System.out.println("Hello, Github Actions!");
+        System.out.println("This build was triggered automatically.");
+    }
+}
